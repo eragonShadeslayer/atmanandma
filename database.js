884d620aa2aa35ef3c82c1d489ec54464599a934
Achyuth KP <achyuthkp94@gmail.com>
2020-10-17 20:34:11 +0200

    removed the lightbox + removed cover image

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ let PersonalData = {
     Title: "Contact",
     Name: "Dr.M.A.Atmanand",
     CurrentPosition: "Visiting Faculty, Indian Institute of Technology, Madras - Chennai",
-    PreviousPosition: "Director (Retd.) <br/>" + "National Institute of Ocean Technology",
+    PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
     OtherRoles: "Chair-IOCINDIO, UNESCO", // <br/> refers to new line. Add this to add a new line in between lines
     Phone: "+91 44 45495146",
-    Email: "atma@niot.res.in",
+    Email: "atma@iitm.ac.in",
     TwitterHandle: "atmanandma",
     LinkedInHandle: "atmanand-m-a-331107a"
 };
