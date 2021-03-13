196b1f6bc5402af67a9af0eade17ef9229fdc021
Achyuth KP <achyuthkp94@gmail.com>
2021-03-13 20:21:44 +0100

    added a new membership role - 2

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let PersonalData = {
     Title: "Contact",
     Name: "Dr.M.A.Atmanand",
-    CurrentPosition: "Visiting Faculty, Indian Institute of Technology, Madras - Chennai",
+    CurrentPosition: "Visiting Professor, Indian Institute of Technology, Madras - Chennai",
     PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
     OtherRoles: "Chair-IOCINDIO, UNESCO", // <br/> refers to new line. Add this to add a new line in between lines
     Phone: "+91 44 45495146",
