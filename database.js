e21afbc5f255d01978c6a7539e8c524b4c1974f7
Achyuth KP <achyuthkp94@gmail.com>
2021-05-19 19:21:36 +0200

    Modified the post to "former" in database and index.html

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ let PersonalData = {
     Name: "Dr.M.A.Atmanand",
     CurrentPosition: "Visiting Professor, Indian Institute of Technology, Madras - Chennai",
     PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
-    OtherRoles: "Chair-IOCINDIO, UNESCO", // <br/> refers to new line. Add this to add a new line in between lines
+    OtherRoles: "Former Chair-IOCINDIO, UNESCO", // <br/> refers to new line. Add this to add a new line in between lines
     Phone: "+91 44 45495146",
     Email: "atma@iitm.ac.in",
     TwitterHandle: "atmanandma",
