cd8811a45e62db08ca968536c840824b5145e5ec
Achyuth KP <achyuthkp94@gmail.com>
2021-11-10 16:00:54 +0100

    added a new role + updated patents, research papers and general info.

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ let PersonalData = {
     Name: "Dr.M.A.Atmanand",
     CurrentPosition: "Visiting Professor, Indian Institute of Technology, Madras - Chennai",
     PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
-    OtherRoles: "Former Chair-IOCINDIO, UNESCO", // <br/> refers to new line. Add this to add a new line in between lines
+    OtherRoles: "Chair, Marine Systems Panel, Naval Research Board, Defence Research and Development Organisation (DRDO), Govt. of India <br/>Former Chair-IOCINDIO, UNESCO", // <br/> refers to new line. Add this to add a new line in between lines
     Phone: "+91 44 45495146",
     Email: "atma@iitm.ac.in",
     TwitterHandle: "atmanandma",
