31fb75cc4da2a7e0cf72a1e5da19c17c3795e751
Achyuth <achyuthkp@knowmore.fr>
2024-09-15 14:41:05 +0200

    Updated the website as per request

--- app.js
+++ app.js
@@ -1,196 +1,219 @@
 /** Load Functions **/
 
 /*
-   * Light YouTube Embeds by @labnol
-   * Credit: https://www.labnol.org/
-   */
+ * Light YouTube Embeds by @labnol
+ * Credit: https://www.labnol.org/
+ */
 
 function labnolIframe(div) {
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
-    iframe.setAttribute('frameborder', '0');
-    iframe.setAttribute('allowfullscreen', '1');
-    iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
-    div.parentNode.replaceChild(iframe, div);
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute(
+    "src",
+    "https://www.youtube.com/embed/" + div.dataset.id + "?autoplay=1&rel=0"
+  );
+  iframe.setAttribute("frameborder", "0");
+  iframe.setAttribute("allowfullscreen", "1");
+  iframe.setAttribute(
+    "allow",
+    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  );
+  div.parentNode.replaceChild(iframe, div);
 }
 
 function initYouTubeVideos() {
-    var playerElements = document.getElementsByClassName('youtube-player');
-    for (var n = 0; n < playerElements.length; n++) {
-        var videoId = playerElements[n].dataset.id;
-        var div = document.createElement('div');
-        div.setAttribute('data-id', videoId);
-        var thumbNode = document.createElement('img');
-        thumbNode.src = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
-        div.appendChild(thumbNode);
-        var playButton = document.createElement('div');
-        playButton.setAttribute('class', 'play');
-        div.appendChild(playButton);
-        div.onclick = function () {
-            labnolIframe(this);
-        };
-        playerElements[n].appendChild(div);
-    }
+  var playerElements = document.getElementsByClassName("youtube-player");
+  for (var n = 0; n < playerElements.length; n++) {
+    var videoId = playerElements[n].dataset.id;
+    var div = document.createElement("div");
+    div.setAttribute("data-id", videoId);
+    var thumbNode = document.createElement("img");
+    thumbNode.src = "//i.ytimg.com/vi/ID/hqdefault.jpg".replace("ID", videoId);
+    div.appendChild(thumbNode);
+    var playButton = document.createElement("div");
+    playButton.setAttribute("class", "play");
+    div.appendChild(playButton);
+    div.onclick = function () {
+      labnolIframe(this);
+    };
+    playerElements[n].appendChild(div);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', initYouTubeVideos);
+document.addEventListener("DOMContentLoaded", initYouTubeVideos);
 
 function addProfilePanel() {
-    const profilePanel = document.getElementById('profilePanel');
-    const imageContainer = createElementAndAppend({
-        tagName: "div",
-        className: "thumb",
-        parentNode: profilePanel
-    });
-    let image = createElementAndAppend({
-        tagName: "img",
-        src: "media/images/photo.jpg",
-        parentNode: imageContainer
-    });
-    let textContainer = createElementAndAppend({
-        tagName: "div",
-        className: "text",
-        parentNode: profilePanel
-    });
-    let textHeader = createElementAndAppend({
-        tagName: "h2",
-        innerText: PersonalData.Name,
-        parentNode: textContainer
-    });
-    let textContent = createElementAndAppend({
-        tagName: "p",
-        innerHTML: PersonalData.CurrentPosition +
-            "<br>" + PersonalData.PreviousPosition +
-            "<br>" + PersonalData.OtherRoles,
-        parentNode: textContainer
-    });
+  const profilePanel = document.getElementById("profilePanel");
+  const imageContainer = createElementAndAppend({
+    tagName: "div",
+    className: "thumb",
+    parentNode: profilePanel,
+  });
+  let image = createElementAndAppend({
+    tagName: "img",
+    src: "media/images/photo.jpg",
+    parentNode: imageContainer,
+  });
+  let textContainer = createElementAndAppend({
+    tagName: "div",
+    className: "text",
+    parentNode: profilePanel,
+  });
+  let textHeader = createElementAndAppend({
+    tagName: "h2",
+    innerText: PersonalData.Name,
+    parentNode: textContainer,
+  });
+  let textContent = createElementAndAppend({
+    tagName: "p",
+    innerHTML:
+      PersonalData.CurrentPosition +
+      "<br>" +
+      PersonalData.PreviousPosition +
+      "<br>" +
+      PersonalData.OtherRoles,
+    parentNode: textContainer,
+  });
 }
 
 function addHeaderPanel() {
-    let headerContainer = document.getElementById('header');
-    let listContainer = createElementAndAppend({
-        tagName: "ul",
-        parentNode: headerContainer
+  let headerContainer = document.getElementById("header");
+  let listContainer = createElementAndAppend({
+    tagName: "ul",
+    parentNode: headerContainer,
+  });
+  let items = [
+    {
+      Name: "Home",
+      Link: "index.html",
+    },
+    {
+      Name: "National Roles",
+      Link: "roles.html",
+    },
+    {
+      Name: "International Roles",
+      Link: "prog.html",
+    },
+    {
+      Name: "Awards & Achievements",
+      Link: "awards.html",
+    },
+    {
+      Name: "Publications",
+      Link: "pub.html",
+    },
+    {
+      Name: "Gallery",
+      Link: "gallery.html",
+    },
+  ];
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i];
+    let listItem = createElementAndAppend({
+      tagName: "li",
+      parentNode: listContainer,
     });
-    let items = [
-        {
-            Name: "Home",
-            Link: "index.html"
-        },
-        {
-            Name: "National Roles",
-            Link: "roles.html"
-        },
-        {
-            Name: "International Roles",
-            Link: "prog.html"
-        },
-        {
-            Name: "Awards & Achievements",
-            Link: "awards.html",
-        },
-        {
-            Name: "Publications",
-            Link: "pub.html"
-        },
-        {
-            Name: "Gallery",
-            Link: "gallery.html"
-        }];
-    for (let i = 0; i < items.length; i++) {
-        let item = items[i];
-        let listItem = createElementAndAppend({
-            tagName: "li",
-            parentNode: listContainer
-        });
-        let linkItem = createElementAndAppend({
-            tagName: "a",
-            innerText: item.Name,
-            href: item.Link,
-            parentNode: listItem
-        });
-        addActiveForPanelLink(linkItem);
-    }
+    let linkItem = createElementAndAppend({
+      tagName: "a",
+      innerText: item.Name,
+      href: item.Link,
+      parentNode: listItem,
+    });
+    addActiveForPanelLink(linkItem);
+  }
 }
 
 function addSidePanel() {
-    let sidePanelContainer = document.getElementById('sidebar');
-    let header = createElementAndAppend({
-        tagName: "h2",
-        innerText: "Other Contributions",
-        parentNode: sidePanelContainer
+  let sidePanelContainer = document.getElementById("sidebar");
+  let header = createElementAndAppend({
+    tagName: "h2",
+    innerText: "Other Contributions",
+    parentNode: sidePanelContainer,
+  });
+  let listContainer = createElementAndAppend({
+    tagName: "ul",
+    parentNode: sidePanelContainer,
+  });
+  let items = [
+    {
+      Name: "Technology Transferred",
+      Link: "tech.html",
+    },
+    {
+      Name: "Invited Talks",
+      Link: "talks.html",
+    },
+    {
+      Name: "Memberships",
+      Link: "ships.html",
+    },
+    {
+      Name: "Student Development",
+      Link: "deve.html",
+    },
+    {
+      Name: "Large Scale Infrastructure Development",
+      Link: "large.html",
+    },
+  ];
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i];
+    let listItem = createElementAndAppend({
+      tagName: "li",
+      parentNode: listContainer,
     });
-    let listContainer = createElementAndAppend({
-        tagName: "ul",
-        parentNode: sidePanelContainer
+    let linkItem = createElementAndAppend({
+      tagName: "a",
+      innerText: item.Name,
+      href: item.Link,
+      parentNode: listItem,
     });
-    let items = [
-        {
-            Name: "Technology Transferred",
-            Link: "tech.html"
-        },
-        {
-            Name: "Invited Talks",
-            Link: "talks.html"
-        },
-        {
-            Name: "Memberships",
-            Link: "ships.html"
-        },
-        {
-            Name: "Student Development",
-            Link: "deve.html"
-        },
-        {
-            Name: "Large Scale Infrastructure Development",
-            Link: "large.html"
-        }];
-    for (let i = 0; i < items.length; i++) {
-        let item = items[i];
-        let listItem = createElementAndAppend({
-            tagName: "li",
-            parentNode: listContainer
-        });
-        let linkItem = createElementAndAppend({
-            tagName: "a",
-            innerText: item.Name,
-            href: item.Link,
-            parentNode: listItem
-        });
-        addActiveForPanelLink(linkItem);
-    }
+    addActiveForPanelLink(linkItem);
+  }
 }
 
 function addFooter() {
-    let footerContainer = document.getElementById('footer');
-    let header = createElementAndAppend({
-        tagName: "h2",
-        innerText: PersonalData.Title,
-        parentNode: footerContainer
-    });
-    let footerInfo = createElementAndAppend({
-        tagName: "p",
-        innerHTML: PersonalData.Name +
-            "<br>" + PersonalData.CurrentPosition +
-            "<br> Ph:" + PersonalData.Phone +
-            "<br><a href='mailto:" + PersonalData.Email + "'> Email:" + PersonalData.Email + "</a>",
-        parentNode: footerContainer
-    });
-    let socialMediaContainer = createElementAndAppend({
-        tagName: "center",
-        innerHTML: "<a target='_blank' href='https://twitter.com/" + PersonalData.TwitterHandle + "' class='fa fa-twitter'></a>" +
-            "<a target='_blank' href='http://www.linkedin.com/in/" + PersonalData.LinkedInHandle + "' class='fa fa-linkedin'></a>",
-        parentNode: footerContainer
-    })
+  let footerContainer = document.getElementById("footer");
+  let header = createElementAndAppend({
+    tagName: "h2",
+    innerText: PersonalData.Title,
+    parentNode: footerContainer,
+  });
+  let footerInfo = createElementAndAppend({
+    tagName: "p",
+    innerHTML:
+      PersonalData.Name +
+      "<br>" +
+      PersonalData.CurrentPosition +
+      "<br> Ph:" +
+      PersonalData.Phone +
+      "<br><a href='mailto:" +
+      PersonalData.Email +
+      "'> Email:" +
+      PersonalData.Email +
+      "</a>",
+    parentNode: footerContainer,
+  });
+  let socialMediaContainer = createElementAndAppend({
+    tagName: "center",
+    innerHTML:
+      "<a target='_blank' href='https://twitter.com/" +
+      PersonalData.TwitterHandle +
+      "' class='fa fa-twitter'></a>" +
+      "<a target='_blank' href='http://www.linkedin.com/in/" +
+      PersonalData.LinkedInHandle +
+      "' class='fa fa-linkedin'></a>",
+    parentNode: footerContainer,
+  });
 }
 
 /** Event Listeners **/
 
 function addActiveForPanelLink(linkItem) {
-    let pathname = document.location.pathname;
-    if (pathname === linkItem.pathname) {
-        linkItem.parentNode.classList.add('active');
-    }
+  let pathname = document.location.pathname;
+  if (pathname === linkItem.pathname) {
+    linkItem.parentNode.classList.add("active");
+  }
 }
 
 /**
@@ -212,35 +235,44 @@ function addActiveForPanelLink(linkItem) {
  * @return {HTMLElement|Element} element - The created element
  */
 createElementAndAppend = function (data, position) {
-    if (!data.tagName) {
-        new Error("Cannot create element : provided tagname is invalid or null");
-    }
-    var element = document.createElement(data.tagName);
-    for (var key in data) {
-        if (data.hasOwnProperty(key) && key === "dataset") {
-            for (var innerDataSetKey in data[key]) {
-                if (data[key].hasOwnProperty(innerDataSetKey)) {
-                    element[key][innerDataSetKey] = data[key][innerDataSetKey];
-                }
-            }
+  if (!data.tagName) {
+    new Error("Cannot create element : provided tagname is invalid or null");
+  }
+  var element = document.createElement(data.tagName);
+  for (var key in data) {
+    if (data.hasOwnProperty(key) && key === "dataset") {
+      for (var innerDataSetKey in data[key]) {
+        if (data[key].hasOwnProperty(innerDataSetKey)) {
+          element[key][innerDataSetKey] = data[key][innerDataSetKey];
         }
-        if (data.hasOwnProperty(key) && key !== "tagName" && key !== "eventListener" && key !== "eventListenerType" && key !== "parentNode") {
-            element[key] = data[key];
-        }
+      }
     }
-    if (data.eventListener) element.addEventListener(data.eventListenerType || "click", data.eventListener);
-    if (data.parentNode) data.parentNode.insertAdjacentElement((position || "beforeend"), element);
-    else console.log("Error : Missing parentNode argument");
-    return element;
+    if (
+      data.hasOwnProperty(key) &&
+      key !== "tagName" &&
+      key !== "eventListener" &&
+      key !== "eventListenerType" &&
+      key !== "parentNode"
+    ) {
+      element[key] = data[key];
+    }
+  }
+  if (data.eventListener)
+    element.addEventListener(
+      data.eventListenerType || "click",
+      data.eventListener
+    );
+  if (data.parentNode)
+    data.parentNode.insertAdjacentElement(position || "beforeend", element);
+  else console.log("Error : Missing parentNode argument");
+  return element;
 };
 
-
-window.addEventListener('load', onLoad);
-
+window.addEventListener("load", onLoad);
 
 function onLoad() {
-    addProfilePanel();
-    addFooter();
-    addHeaderPanel();
-    addSidePanel();
+  addProfilePanel();
+  addFooter();
+  addHeaderPanel();
+  addSidePanel();
 }
