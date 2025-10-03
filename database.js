3c30c857e68be429c6b4ad46367eaaf615ee1b16
Achyuth <achyuthkp@knowmore.fr>
2025-10-03 17:42:44 +0200

    Updated personal data and added new images to the gallery, including photos from upcoming conferences.

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 const PersonalData = {
-    Title: "Contact",
-    Name: "Dr.M.A.Atmanand",
-    CurrentPosition: "Visiting Professor, Indian Institute of Technology, Madras - Chennai",
-    PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
-    OtherRoles: "Chair, Marine Systems Panel, Naval Research Board, Defence Research and Development Organisation (DRDO), Govt. of India <br/>Former Chair-IOCINDIO, UNESCO", // <br/> refers to new line. Add this to add a new line in between lines
-    Email: "atma@iitm.ac.in",
-    TwitterHandle: "atmanandma",
-    LinkedInHandle: "atmanand-m-a-331107a"
+  Title: "Contact",
+  Name: "Dr.M.A.Atmanand",
+  CurrentPosition:
+    "Guest Professor, Indian Institute of Technology, Madras - Chennai <br/> Visiting Professor at the National Institute of Technology (NIT), Calicut",
+  PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
+  OtherRoles:
+    "Chair, Marine Systems Panel, Naval Research Board, Defence Research and Development Organisation (DRDO), Govt. of India <br/>Former Chair-IOCINDIO, UNESCO",
+  Email: "atma@iitm.ac.in",
+  TwitterHandle: "atmanandma",
+  LinkedInHandle: "atmanand-m-a-331107a",
 };
