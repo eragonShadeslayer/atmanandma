0d11af99acea95365736893c5d57e8f9eb353219
Achyuth <achyuthkp@knowmore.fr>
2025-10-17 13:45:26 +0200

    Refactor personal data formatting and update current position details

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const PersonalData = {
   Title: "Contact",
   Name: "Dr.M.A.Atmanand",
   CurrentPosition:
-    "Guest Professor, Indian Institute of Technology, Madras - Chennai <br/> Visiting Professor at the National Institute of Technology (NIT), Calicut",
+    "Guest Professor, Indian Institute of Technology, Madras - Chennai<br/>Visiting Professor at the National Institute of Technology (NIT), Calicut",
   PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
   OtherRoles:
-    "Chair, Marine Systems Panel, Naval Research Board, Defence Research and Development Organisation (DRDO), Govt. of India <br/>Former Chair-IOCINDIO, UNESCO",
+    "Chair, Marine Systems Panel, Naval Research Board, Defence Research and Development Organisation (DRDO), Govt. of India<br/>Former Chair-IOCINDIO, UNESCO",
   Email: "atma@iitm.ac.in",
   TwitterHandle: "atmanandma",
   LinkedInHandle: "atmanand-m-a-331107a",
