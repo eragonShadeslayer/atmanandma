5eddf5d7f008af49b1a9b4a3aa48ab79d8bedfd5
Achyuth <achyuthkp@knowmore.fr>
2026-03-30 11:28:30 +0200

    updated job title and years

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const PersonalData = {
   Title: "Contact",
   Name: "Dr.M.A.Atmanand",
   CurrentPosition:
-    "Guest Professor, Indian Institute of Technology, Madras - Chennai<br/>Visiting Professor at the National Institute of Technology (NIT), Calicut",
+    "Adjunct Professor, Indian Institute of Technology, Madras - Chennai<br/>Visiting Professor at the National Institute of Technology (NIT), Calicut",
   PreviousPosition: "Director (Retd.), National Institute of Ocean Technology",
   OtherRoles:
     "Chair, Marine Systems Panel, Naval Research Board, Defence Research and Development Organisation (DRDO), Govt. of India<br/>Former Chair-IOCINDIO, UNESCO",
